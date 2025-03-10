de1e868f4b67d678aabab6d936cec808554101ba
RyanYavari <ryanyavari10@gmail.com>
2025-03-10 04:00:20 -0700

    Removed node_modules from tracking

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
+import productRoutes from "./routes/product.route.js"
+
+
+
+
+/*
+    In this method we will create our API through POST, GET, PUT, and DELETE. These commands will make requests and retrieve responses with our MongoDB database. 
+*/
+
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use(express.json()); //allows us to accept JSON data in the req.body
 
+
+
+app.use("/api/products", productRoutes)
+
+
+// Connect database
 
 app.listen(8000, () => {
     connectDB();
